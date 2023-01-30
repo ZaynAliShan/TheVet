71bb706f61487f74eb3ca6d1e6d7861f96cd4d80
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-01-30 21:21:14 +0500

    concurrent Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Doctors from "./components/Doctors";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Appointments from "./components/Appointments";
-// import Signup from "./components/Signup";
+import Signup from "./components/Signup";
 import Blog from "./components/Blog";
 import FAQ from "./components/FAQs";
 import Footer from "./components/Footer";
@@ -15,11 +17,12 @@ function App() {
       <Doctors />
       <Services />
       <Appointments />
-      {/* <Signup /> */}
-      <Blog/>
-      <FAQ/>
-      <Footer/>
-
+      <Signup />
+      <Blog />
+      <FAQ />
+      <About />
+      <Contact />
+      <Footer />
     </>
   );
 }
