4facf917a1213e9d67949201e91ea26b387512f6
zzaynAli <bcsf19a022@pucit.edu.pk>
2023-06-10 16:30:11 +0500

    Updated Mongoose

--- Backend/db.js
+++ Backend/db.js
@@ -1,33 +1,13 @@
-// //1. first we want to connect to our database using mongoose
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/TheVet";
 
-// const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/TheVet"
-
-// make function async and it waits for the promise to resolve wherever we put await
-
-const connect_to_mongodb = () => {
-  mongoose.connect(mongoURI, ()=> { // when mongo connects this call back function gets fired
+const connect_to_mongodb = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Mongo Connection Established!")
-  })
+  } catch (err) {
+    console.error(`Error in MongoDB connection: ${err}`);
+  }
 }
 
-// now we want to export our this modeule to the index.js where we will perform connection
 module.exports = connect_to_mongodb;
-
-const mongoose = require('mongoose');
-
-// const connect_to_mongodb = () => {
-//   mongoose
-//     .connect(mongoURI, {
-
-//     })
-//     .then(() => {
-//       console.log('Database connection successful');
-//     })
-//     .catch((error) => {
-//       console.error('Database connection error:', error);
-//       process.exit(1);
-//     });
-// };
-
-//module.exports = connect_to_mongodb;
